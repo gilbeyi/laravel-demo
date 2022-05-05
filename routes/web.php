85327f70d93b1004105c5b160149aa848c994174
r.i <pechuelii33@gmail.com>
2022-05-05 23:51:15 +0900

    add:blade sample

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users/{name}', function($name) {
+    return view('users', ['name'=> $name]);
+});
